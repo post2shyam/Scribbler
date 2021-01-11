6aaee83ee3519eef57fc14f493043159dee6de5a
Shyam Grover <post2shyam@gmail.com>
2021-01-11 10:46:58 +0000

    Add dummy signup modal dialog

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,27 @@
-function handleSignUpButtonClick() {
-    alert("Sign up clicked");
-};
+ // When the user clicks on the signup button, open the modal
+ function handleSignUpButtonClick() {
 
-function handleSignInButtonClick() {
-    alert("Sign in clicked");
-};
+     // Get the modal
+     var modal = document.getElementById("signUpModal");
+
+     // Get the <span> element that closes the modal
+     var span = document.getElementsByClassName("close")[0];
+
+     // When the user clicks on <span> (x), close the modal
+     span.onclick = function() {
+         modal.style.display = "none";
+     };
+
+     // When the user clicks anywhere outside of the modal, close it
+     window.onclick = function(event) {
+         if (event.target == modal) {
+             modal.style.display = "none";
+         }
+     };
+
+     modal.style.display = "block";
+ };
+
+ function handleSignInButtonClick() {
+     alert("Sign in clicked");
+ };

5c7db0f3d21cac668965693e3d550b44b64af9f4
Shyam Grover <post2shyam@gmail.com>
2021-01-12 18:27:40 +0530

    Add dummy CreatePost dialog

--- scripts/index.js
+++ scripts/index.js
@@ -42,3 +42,30 @@
 
      modal.style.display = "block";
  };
+
+
+ function handleAllPostsButtonClick() {
+     alert("navigate to bloglist.html");
+ }
+
+ function handleCreatePostButtonClick() {
+     // Get the modal
+     var modal = document.getElementById("createPostModal");
+
+     // Get the <span> element that closes the modal
+     var span = document.getElementById("createPostClose");
+
+     // When the user clicks on <span> (x), close the modal
+     span.onclick = function() {
+         modal.style.display = "none";
+     };
+
+     // When the user clicks anywhere outside of the modal, close it
+     window.onclick = function(event) {
+         if (event.target == modal) {
+             modal.style.display = "none";
+         }
+     };
+
+     modal.style.display = "block";
+ }

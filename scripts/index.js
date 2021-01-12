7f951b9e1b3c69b2e74920af7327cbf53bdf1401
Shyam Grover <post2shyam@gmail.com>
2021-01-12 09:01:32 +0000

    Add singin modal dialog

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,10 @@
  // When the user clicks on the signup button, open the modal
  function handleSignUpButtonClick() {
-     console.log("sdfd");
-
      // Get the modal
      var modal = document.getElementById("signUpModal");
 
      // Get the <span> element that closes the modal
-     var span = document.getElementsByClassName("close")[0];
+     var span = document.getElementById("signUpClose");
 
      // When the user clicks on <span> (x), close the modal
      span.onclick = function() {
@@ -24,5 +22,23 @@
  };
 
  function handleSignInButtonClick() {
-     alert("Sign in clicked");
+     // Get the modal
+     var modal = document.getElementById("signInModal");
+
+     // Get the <span> element that closes the modal
+     var span = document.getElementById("signInClose");
+
+     // When the user clicks on <span> (x), close the modal
+     span.onclick = function() {
+         modal.style.display = "none";
+     };
+
+     // When the user clicks anywhere outside of the modal, close it
+     window.onclick = function(event) {
+         if (event.target == modal) {
+             modal.style.display = "none";
+         }
+     };
+
+     modal.style.display = "block";
  };
